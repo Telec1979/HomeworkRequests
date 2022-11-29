c6c07e8ffab7652574c9279cdae1f05092afa2be
Denis Upadyshev <telec1979@yandex.ru>
2022-11-29 23:17:51 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import requests
-response1 = requests.get('https://akabab.github.io/superhero-api/api/all.json')
-names_list = ['Hulk', 'Captain America', 'Thanos']
-max_int = 0
-name = ''
-for item in response1.json():
-    if item['name'] in names_list:
-        if item['powerstats']['intelligence'] > max_int:
-            max_int = item['powerstats']['intelligence']
-            name = item['name']
-print(f'{name} is clever!!!')
+import os
+
+
+class YaUploader:
+
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск в корневую папку"""
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload/'  # задаем адрес, по которому будем делать запрос
+        # объявляем обязательные параметры метода - путь/имяфайла, куда и с каким названием на ЯДиск будем загружать
+        params = {'path': file_path} 
+        # делаем запрос на получение ссылки на загрузку файла
+        res = requests.get(url, headers=self.get_headers(), params=params)  
+        # получаем из ответа ссылку для загрузки файла
+        upload_url = res.json()['href']
+        # на эту ссылку отправляем put запрос с указанием параметра data - файла, открытого для чтения
+        response = requests.put(upload_url, data=open(path_file, 'rb'), headers=self.get_headers())
+        if response.status_code == 201:
+            print('Файл загружен успешно')
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path = input('Укажите абсолютный путь к загружаемому файлу: ')
+    name_of_file = os.path.basename(path)
+    path_file = os.path.join(os.getcwd(), path)
+    token = input('Введите свой токен: ')
+    uploader = YaUploader(token)
+    result = uploader.upload(name_of_file)
